2c39dd1e5ff60b9b2f8bc0c80d4304e87251394c
MarkNahabedian <naha@mit.edu>
2019-01-26 15:13:59 -0500

    Understand special values of length.  First cut at box model.

--- check_jp2.py
+++ check_jp2.py
@@ -20,14 +20,10 @@ def scan_jp2(filename):
     print('Total size: %d' % filelength)
     with open(filename, 'rb') as f:
         while f.tell() < filelength:
-            start = f.tell()
-            header = f.read(8)
-            section_length = big_endian_32(header)
-            tag = get_tag(header)
-            print('%8d: %8d %s    %r' % (start, section_length, tag, header))
-            if section_length <= 0:
-                break
-            f.seek(start + section_length, os.SEEK_SET)
+            box = readBox(f)
+            print('%8d: %8d %s  %s' % (
+                box.box_start, box.box_size, box.box_type, box.__class__.__name__))
+            f.seek(box.box_end, os.SEEK_SET)
         if f.tell() < filelength:
             print('Scan terminated abnormally.')
 
@@ -37,9 +33,53 @@ def get_tag(header):
         result += chr(header[index])
     return result
 
-def big_endian_32(header):
+def big_endian_int(buffer, offset=0, bytecount = 4):
     result = 0
-    for index in range(0, 4):
+    for index in range(offset, offset + bytecount):
         result = result << 8
-        result += header[index]
+        result += buffer[index]
     return result
+
+def readBox(f):
+    box_start = f.tell()
+    header = f.read(8)
+    box_size = big_endian_int(header)
+    if box_size == 1:
+        xlbox = f.read(8)
+        box_size = big_endian_int(xlbox, bytecount=8)
+    elif box_size == 0:
+        box_size = os.fstat(f.fileno()).st_size - box_start
+    box_type = get_tag(header)
+    return JP2Box.class_for_box_type(box_type)(box_type, box_start, box_size)
+
+
+class JP2Box (object):
+    '''JP2Box is both the superclass of all boxes and the defauklt
+    imlementation for boxes we don't have a more specific
+    implementation for,'''
+    box_type = None
+    isContainer = None
+
+    @classmethod
+    def class_for_box_type(cls, box_type):
+        def walk(cls):
+            if cls.box_type == box_type:
+                return cls
+            for sc in cls.__subclasses__():
+                found = walk(sc)
+                if found != None:
+                    return found
+            return None
+        return walk(cls) or JP2Box
+
+    def __init__(self, box_type, box_start, box_size):
+        self.box_type = box_type
+        self.box_start = box_start
+        self.box_size = box_size
+
+    @property
+    def box_end(self):
+        return self.box_start + self.box_size
+
+
+
