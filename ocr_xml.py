1aa8d2a0ea00ec909b936e2359c2934a5c450713
MarkNahabedian <naha@mit.edu>
2019-01-30 17:11:55 -0500

    Page by page width, height and dpi.

--- ocr_xml.py
+++ ocr_xml.py
@@ -33,3 +33,22 @@ def get_page_boundaries(filename):
     print('Y: ', minY, maxY)
 
 
+def page_by_page(filename):
+    tree = ET.parse(filename)
+    count = 0
+    for page in tree.iter('OBJECT'):
+        count += 1
+        height = int(page.attrib['height'])
+        width = int(page.attrib['width'])
+        # Maybe also look at first and last REGION of page.
+        # get file and DPI
+        page_file = None
+        dpi = None
+        for param in page.iter('PARAM'):
+            if param.attrib['name'] == "PAGE":
+                page_file = param.attrib['value']
+            elif param.attrib['name'] == 'DPI':
+                dpi = int(param.attrib['value'])
+        print("%dw %dh %ddpi %s" % (width, height, dpi, page_file))
+    print('%d pages' % count)
+
