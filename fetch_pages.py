fdcf5ad8055f95d660036dfdc061bc2931a7bdfe
MarkNahabedian <naha@mit.edu>
2019-05-21 11:00:19 -0400

    Fetch abbyy XML file since it should tell us where pictures are.

--- fetch_pages.py
+++ fetch_pages.py
@@ -7,7 +7,9 @@ import os
 import os.path
 import archive_org
 import zipfile
+import gzip
 import page
+from collections import namedtuple
 from write_html import write_html
 
 
@@ -25,7 +27,7 @@ archive.org URI.  For example, for the book archived as
 
 the command line argument would be "hartnessflatturr00unse".
 
-A subdirectory with that name will be created in the workiing
+A subdirectory with that name will be created in the working
 directory and the page images and metadata will be downloaded there.
 
 An HTML file will also be created in that directory that enumerates
@@ -46,13 +48,50 @@ def main():
         b.make_thumbnails()
         write_html(b)
 
-# ??? Should we be working with the processed or the original jp2 files?
-PAGES_FORMAT = 'Single Page Processed JP2 ZIP'
+
+DownloadFormat = namedtuple('DownLoadFormat', ('format', 'action'))
+
+
+def extract_jp2_archive(file_metadata, fetched_file, target_directory):
+    assert file_metadata['format'] == 'Single Page Processed JP2 ZIP'
+    pages_dir = os.path.join(target_directory, 'pages')
+    os.mkdir(pages_dir)
+    zf = zipfile.ZipFile(fetched_file, 'r')
+    for page in zf.namelist():
+        zf.extract(page, path=pages_dir)
+
+
+def unzib_abbyy(file_metadata, fetched_file, target_directory):
+    print(fetched_file)
+    assert file_metadata['format'] == 'Abbyy GZ'
+    f = gzip.open(fetched_file, 'rb')
+    directory, filename = os.path.split(fetched_file)
+    base = filename.rsplit('.', 1)[0]
+    outfile = os.path.join(directory, base + '.xml')
+    with open(outfile, 'wb') as out:
+        while True:
+            buffer = f.read(10000)
+            if len(buffer) == 0:
+                break
+            out.write(buffer)
+
 
 DOWNLOAD_FORMATS = [
-    'Metadata', 'Djvu XML', 'Dublin Core',
-    PAGES_FORMAT
+    DownloadFormat('Metadata', None),
+    DownloadFormat('Djvu XML', None),
+    DownloadFormat('Dublin Core', None),
+    DownloadFormat('Abbyy GZ', unzib_abbyy),
+    # ??? Should we be working with the processed or the original jp2 files?
+    DownloadFormat('Single Page Processed JP2 ZIP', extract_jp2_archive)
     ]
+
+
+def want_download(format):
+    for df in DOWNLOAD_FORMATS:
+        if df.format == format:
+            return df
+    return None
+
 
 def fetch_book(book):
     d = os.path.join(os.path.abspath(os.curdir), book)
@@ -65,20 +104,18 @@ def fetch_book(book):
     remote_files = metadata['files']
     for f in remote_files:
         this_file = None
-        if f['format'] in DOWNLOAD_FORMATS:
-            this_file = os.path.join(d, f['name'])
-            archive_org.fetch_url_to_file(
-                archive_org.FILE_FETCH_URL_TEMPLATE.format(
-                    DIR=remote_dir,
-                    NAME=f['name']),
-                this_file, binary=True)
-            print('Wrote', this_file)
-        if f['format'] == PAGES_FORMAT:
-            pages_dir = os.path.join(d, 'pages')
-            os.mkdir(pages_dir)
-            zf = zipfile.ZipFile(this_file, 'r')
-            for page in zf.namelist():
-                zf.extract(page, path=pages_dir)
+        download_format = want_download(f['format'])
+        if download_format == None:
+            continue
+        this_file = os.path.join(d, f['name'])
+        archive_org.fetch_url_to_file(
+            archive_org.FILE_FETCH_URL_TEMPLATE.format(
+                DIR=remote_dir,
+                NAME=f['name']),
+            this_file, binary=True)
+        print('Wrote', this_file)
+        if download_format.action != None:
+            download_format.action(f, this_file, d)
     print('Book downloaded to', d)
 
 
